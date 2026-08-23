4a2adec4657fbba7e7b12cbfa107955aa80a0709
CoodeNoob <pg@PGs-MacBook-Air.local>
2026-08-23 15:59:16 +0530

    context_init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import LoginPage from './pages/Auth/Login/LoginPage'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import LoginPage from "./pages/Auth/Login/LoginPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
+
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const { isAuthenticated } = useAuth();
+
   return (
-    <div>
-      <LoginPage/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/dashboard" />
+            ) : (
+              <LoginPage />
+            )
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            isAuthenticated ? (
+              <Dashboard />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to={isAuthenticated ? "/dashboard" : "/login"}
+            />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
